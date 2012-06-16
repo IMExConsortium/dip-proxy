a32e7fd278d8daf10e792d166d2d57ecae591218
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-06-16 00:46:50 +0000

    clean up in dip-proxy-api.

--- dip-proxy-api/src/main/java/edu/ucla/mbi/proxy/FaultFactory.java
+++ dip-proxy-api/src/main/java/edu/ucla/mbi/proxy/FaultFactory.java
@@ -15,10 +15,21 @@ import edu.ucla.mbi.services.ServiceFault;
 
 public class FaultFactory {
 
-    private static final String MESSAGE = "ProxyFault";
+    private static final String MESSAGE = "ProxyFault:";
     
     public static ProxyFault newInstance( int code ) {
-        return new ProxyFault( MESSAGE, Fault.getServiceFault( code ) );
+        ServiceFault serviceFault = new ServiceFault();
+        serviceFault.setFaultCode( code );
+        
+        String message;
+        if( Fault.fault.get(code) != null ) {
+            message = Fault.fault.get( code );
+        } else {
+            message = Fault.fault.get( 99 );
+        }
+        serviceFault.setMessage( message );
+
+        return new ProxyFault( MESSAGE, serviceFault);
     }
 
     public static ProxyFault newInstance( ServiceFault fault ) {
