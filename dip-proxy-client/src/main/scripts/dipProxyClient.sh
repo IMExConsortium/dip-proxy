4a9f07a54a79f13bc68057920a6d785f0a72063a
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-06-25 19:09:09 +0000

    clean up in dip-proxy trunk.

--- dip-proxy-client/src/main/scripts/dipProxyClient.sh
+++ dip-proxy-client/src/main/scripts/dipProxyClient.sh
@@ -4,7 +4,3 @@ if [ "$1" = "proxyM" ]; then
 java -cp ../../../target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.ProxyServiceClient http://10.1.1.201:8080/dip-proxy-server/proxy-service $2 $3 $4 $5 $6 $7 $8 $9 $10
 fi
 
-if [ "$1" = "legacyM" ]; then
-#java -cp ../dip-proxy-api/target/dip-proxy-api.jar edu.ucla.mbi.client.DipLegacyClient http://dip.doe-mbi.ucla.edu/dip-legacy/dxf $2 $3 $4 $5 $6 $7 $8 $9 $10
-fi
-
