0c38ea20b47f833b51a9d1bca5c059f4f88de3bb
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-08-17 19:46:34 +0000

    clean up in dip-proxy-server.

--- dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/RemoteProxyServer.java
+++ dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/RemoteProxyServer.java
@@ -24,7 +24,11 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBContext;
 
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.datatype.XMLGregorianCalendar;
 
+import javax.xml.ws.Holder;
+import javax.xml.ws.BindingProvider;
+import com.sun.xml.ws.developer.JAXWSProperties;
 
 import edu.ucla.mbi.dxf14.DatasetType;
 import edu.ucla.mbi.dxf14.DxfJAXBContext;
@@ -36,9 +40,10 @@ import edu.ucla.mbi.cache.NativeRecord;
 import java.util.Map;
 
 public class RemoteProxyServer implements RemoteServer {
- 
+
+    Log log = LogFactory.getLog( RemoteProxyServer.class ); 
     String proxyAddress;
-    private Log log = LogFactory.getLog( RemoteProxyServer.class );
+
     private static Map<String,Object> context;
 
     public boolean isNative(){
@@ -57,7 +62,7 @@ public class RemoteProxyServer implements RemoteServer {
     }
     
     public void setContext( Map<String,Object> context ) {
-        RemoteProxyServer.context = context;
+        this.context = context;
     }
     
     public Map<String,Object> getContext() {
@@ -65,9 +70,63 @@ public class RemoteProxyServer implements RemoteServer {
     }
     
     public NativeRecord getNative( String provider, String service,
-                             String ns, String ac, int timeout
-                             ) throws ProxyFault {
-        return null;
+                                   String ns, String ac, int timeout
+                                   ) throws ProxyFault 
+    {
+
+        log.info( "getNative(NS=" + ns + " AC=" + ac + " OP=" + service + ")" );
+
+        // call EBI proxy localized at ProxyAddress
+
+        String url = getAddress();
+
+        ProxyService proxySrv = new ProxyService();
+        ProxyPort port = proxySrv.getProxyPort();
+
+        try {
+            log.info( " this=" + this );
+            log.info( " port=" + url + " timeout=" + timeout );
+
+            // set server location
+            // ---------------------
+
+            ((BindingProvider) port).getRequestContext().put(
+                    BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url );
+
+            // set client Timeout
+            // ------------------
+
+            ((BindingProvider) port).getRequestContext().put(
+                   JAXWSProperties.CONNECT_TIMEOUT, timeout );
+
+            Holder<DatasetType> resDataset = new Holder<DatasetType>();
+            Holder<String> resNative = new Holder<String>();
+            Holder<XMLGregorianCalendar> timestamp =
+                                new Holder<XMLGregorianCalendar>();
+
+            port.getRecord( provider, service, ns, ac, "", "", "native", "", 0,
+                            timestamp, resDataset, resNative );
+
+            XMLGregorianCalendar qtime = timestamp.value;
+
+            NativeRecord record = new NativeRecord( provider, service, ns, ac );
+            record.setNativeXml( resNative.value );
+            record.setCreateTime( qtime.toGregorianCalendar().getTime() );
+
+            return record;
+
+        } catch ( ProxyFault fault ) {
+            throw fault;
+        } catch ( Exception e ) {
+            log.info( "getNative: exception: " + e.toString() );
+            if ( e.toString().contains( "No result found" ) ) {
+                throw FaultFactory.newInstance( Fault.NO_RECORD ); // no hits
+            } else if ( e.toString().contains( "Read timed out" ) ) {
+                throw FaultFactory.newInstance( Fault.REMOTE_TIMEOUT ); // timeout
+            } else {
+                throw FaultFactory.newInstance( Fault.UNKNOWN ); // unknown
+            }
+        }
     }
 
     public RemoteServer getRemoteServerInstance() {
@@ -86,12 +145,10 @@ public class RemoteProxyServer implements RemoteServer {
 
 
     public DatasetType transform( String strNative,
-				  String ns, String ac, String detail,
-				  String service, ProxyTransformer pTrans 
-                                  ) throws ProxyFault 
-    {
+				                  String ns, String ac, String detail,
+				                  String service, ProxyTransformer pTrans 
+                                  ) throws ProxyFault {
 	
-        //Log log = LogFactory.getLog( RemoteProxyServer.class );
 	    try {
 	        // native data in string representationa as input
 	    
@@ -109,6 +166,7 @@ public class RemoteProxyServer implements RemoteServer {
 	    
 	        pTrans.setTransformer( service );
     	    pTrans.setParameters( detail, ns, ac );
+            log.info( "transform: before transform.");
     	    pTrans.transform( ssNative, result );
 	    
 	        DatasetType dxfResult  = 
@@ -116,33 +174,28 @@ public class RemoteProxyServer implements RemoteServer {
 	    
             //test if dxfResult is empty
 	        if ( dxfResult.getNode().isEmpty() ) {
-		        //ServiceFault fault = 
-		        //new ServiceFault( "05 remote server: transformer." );
-		        throw FaultFactory.newInstance( Fault.UNKNOWN ) ; 
+		        throw FaultFactory.newInstance( Fault.TRANSFORM ); 
 	        }	    
 	    
 	        return dxfResult;
 	    
-	    } catch ( ProxyFault fault) { 
-	        log.info( "Transformer fault:" + fault.getFaultInfo().getMessage() );
+	    } catch ( ProxyFault fault ) { 
+	        log.info( "Transformer fault: empty dxfResult ");
 	        throw fault;
         } catch ( Exception e ) {
 	        log.info( "Exception="+e.toString() );
-	        //ServiceFault fault = 
-	        //	new ServiceFault( "05 remote server: transform." ); 
-	        throw FaultFactory.newInstance( Fault.UNKNOWN );  // 
+	        throw FaultFactory.newInstance( Fault.TRANSFORM );  
 	    }   
     }
     
     public DatasetType buildDxf( String strNative, String ns, String ac,
 				                 String detail, String service, 
 				                 ProxyTransformer pTrans 
-                                 ) throws ProxyFault 
-    {
+                                 ) throws ProxyFault {
 	
     	// NOTE: overload if dxf building more complex than
-	    // a simple xslt transformation
-        log.info( "buildDxf entering ... ");	
+	    //       a simple xslt transformation
+        log.info( "buildDxf entering ... " );	
 	    return this.transform( strNative, ns, ac, detail, service, pTrans );
     }
 }
