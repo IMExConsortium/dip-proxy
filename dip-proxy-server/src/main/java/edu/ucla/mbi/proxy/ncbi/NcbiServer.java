08db33a950af6ab390331a23f034e2b766809081
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-09-11 18:19:51 +0000

    update NcbiServer.java in dip-proxy-server.

--- dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/ncbi/NcbiServer.java
+++ dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/ncbi/NcbiServer.java
@@ -80,7 +80,10 @@ public class NcbiServer extends RemoteServerImpl {
                               "url_esearch_string is not configured. " );
                     throw FaultFactory.newInstance( Fault.UNSUPPORTED_OP );
                 }
-
+                 
+                esearch_restAcTag = esearch_restAcTag.replaceAll( "^\\s+", "" );
+                esearch_restAcTag = esearch_restAcTag.replaceAll( "\\s+$", "" );
+                url_esearch_string = url_esearch_string.replaceAll( "\\s", "" );
 
                 url_esearch_string = url_esearch_string.replaceAll( 
                                                     esearch_restAcTag, ac );
@@ -179,6 +182,10 @@ public class NcbiServer extends RemoteServerImpl {
                               "url_efetch_string is not configured. " );
                     throw FaultFactory.newInstance( Fault.UNSUPPORTED_OP );
                 }
+
+                efetch_restAcTag = efetch_restAcTag.replaceAll( "^\\s+", "" );
+                efetch_restAcTag = efetch_restAcTag.replaceAll( "\\s+$", "" );
+                url_efetch_string = url_efetch_string.replaceAll( "\\s", "" );
 
                 url_efetch_string = url_efetch_string.replaceAll( 
                                             efetch_restAcTag, ncbi_nlmid );
