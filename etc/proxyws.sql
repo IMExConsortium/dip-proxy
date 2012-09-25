b6dc5401c484ae3cf9c5e689f5f2eb5f8b6a43bc
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-09-25 21:37:36 +0000

    updaste dip-proxy trunk based on dev2 server address.

--- etc/proxyws.sql
+++ etc/proxyws.sql
@@ -29,7 +29,8 @@ CREATE TABLE native_audit (
     ns character varying(20) DEFAULT ''::character varying NOT NULL,
     ac character varying(20) DEFAULT ''::character varying NOT NULL,
     time timestamp without time zone NOT NULL,
-    delay integer DEFAULT 0 NOT NULL
+    delay integer DEFAULT 0 NOT NULL,
+    status integer DEFAULT 0 NOT NULL 
 );
 
 CREATE INDEX native_idx1 ON native_record (provider, service, ns, ac);
@@ -38,10 +39,7 @@ CREATE INDEX native_idx2 ON native_record (expire_time);
 CREATE INDEX dxf_idx1 ON dxf_record (provider, service, ns, ac, detail);
 CREATE INDEX dxf_idx2 ON dxf_record (expire_time);
 
-CREATE INDEX I11 ON native_audit (service);
-CREATE INDEX I12 ON native_audit (provider, service);
-CREATE INDEX I13 ON native_audit (ns);
-CREATE INDEX I14 ON native_audit (ac, ns);
-CREATE INDEX I15 ON native_audit (time);
-CREATE INDEX I16 ON native_audit (time, provider);
-CREATE INDEX I17 ON native_audit (delay);
+CREATE INDEX I12 ON native_audit (provider, service, ns, ac);
+CREATE INDEX I18 ON native_audit (provider, service, pk);
+CREATE INDEX I19 ON native_audit (provider, service, time);
+
