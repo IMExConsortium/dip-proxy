6f01d1ed8d55a01c2c2095e87b88cf9a3a03d3ca
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2013-04-05 19:49:56 +0000

    add super class StrutsPortImpl.java

---
+++ dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/StrutsPortImpl.java
@@ -0,0 +1,31 @@
+package edu.ucla.mbi.proxy;
+
+/*==============================================================================
+ * $HeadURL$
+ * $Id$
+ * Version: $Rev$
+ *==============================================================================
+ *
+ * StrutsPortImpl - super class used to set WSContext 
+ *                                  
+ *=========================================================================== */
+
+import edu.ucla.mbi.server.WSContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class StrutsPortImpl {
+    
+    protected WSContext context;   
+
+    public void setContext ( WSContext context ) {
+        this.context = context;
+    }     
+
+    public void initialize() {
+
+        Log log = LogFactory.getLog( StrutsPortImpl.class );
+        log.info( "StrutsPortImpl initializing..." );
+    } 
+}
