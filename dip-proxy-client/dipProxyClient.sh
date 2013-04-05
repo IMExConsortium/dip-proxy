2f3fccb0b3177e9ba1689811099b7c6959164dd7
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2013-04-05 19:54:09 +0000

    test old ncbi-service.

--- dip-proxy-client/dipProxyClient.sh
+++ dip-proxy-client/dipProxyClient.sh
@@ -12,10 +12,10 @@ java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.P
 #java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.ProxyCommandClient http://10.1.200.210:8080/dip-proxy/proxy-service $2 $3 $4 $5 $6 $7 $8 $9 $10
 #fi
 
-#if [ "$1" = "ncbi-service" ]; then
-#java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.NcbiServiceClient http://10.1.200.201:8080/dip-proxy-server/ncbi-service $2 $3 $4 $5 $6 $7 $8 $9 $10
+if [ "$1" = "ncbi-service" ]; then
+java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.NcbiServiceClient http://10.1.200.201:8080/dip-proxy/ncbi-service $2 $3 $4 $5 $6 $7 $8 $9 $10
 #java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.ProxyCommandClient http://dip.doe-mbi.ucla.edu:80/dip-proxy/ncbi-service $2 $3 $4 $5 $6 $7 $8 $9 $10
-#fi
+fi
 
 #if [ "$1" = "ebi-service" ]; then
 #java -cp target/dip-proxy-client-jar-with-dependencies.jar edu.ucla.mbi.client.ProxyCommandClient http://10.1.200.201:8080/dip-proxy-server/ebi-service $2 $3 $4 $5 $6 $7 $8 $9 $10
