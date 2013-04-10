878a2edd8998f12188184d908fdb854101410c8d
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2013-04-10 22:00:50 +0000

    recover implements Filter class.

--- dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/ProxyConfigFilter.java
+++ dip-proxy-server/src/main/java/edu/ucla/mbi/proxy/ProxyConfigFilter.java
@@ -20,33 +20,21 @@ import javax.servlet.http.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import edu.ucla.mbi.proxy.router.Dht;
+import edu.ucla.mbi.server.WSContext;
 
-import org.springframework.web.filter.GenericFilterBean;
+public class ProxyConfigFilter  implements Filter{
 
-//public class ProxyConfigFilter  implements Filter{
-public class ProxyConfigFilter extends GenericFilterBean {
-    
-    private Dht dht;
-
-    public void setDht ( Dht dht ) {
-        this.dht = dht;
-    }
-
-    //public void init( FilterConfig config ) throws ServletException{
-    public void initFilterBean() throws ServletException {    
+    public void init( FilterConfig config ) throws ServletException{
         Log log = LogFactory.getLog( this.getClass() );
         log.info( "ProxyConfigFilter: init(config) called");
         
         String path = "";        
 
         try{ 
-            //URL pathUrl = config.getServletContext().getResource("/");
-            URL pathUrl = super.getServletContext().getResource("/");
+            URL pathUrl = config.getServletContext().getResource("/");
             
-            //path = new File( config.getServletContext().getRealPath("/")).getAbsolutePath();
-            path = new File( super.getServletContext().getRealPath("/"))
-                    .getAbsolutePath();
+            path = new File( config.getServletContext().getRealPath("/") )
+                .getAbsolutePath();
 
             log.info( " protocol=" + pathUrl.getProtocol() );
             log.info( " path=" + path );
@@ -55,9 +43,8 @@ public class ProxyConfigFilter extends GenericFilterBean {
         } catch(Exception ex){ ex.printStackTrace();}
 
 
-        //WSContext.getDht().initialize();
-        log.info( "dht=" + dht );
-        dht.initialize();
+        WSContext.getDht().initialize();
+
         log.info( "after dht initialize ");
 
     } 
